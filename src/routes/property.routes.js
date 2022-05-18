9fa4cc310204407a929487e07e8e89decd1ecb8b
Agnes4Him <ojuhagnes@gmail.com>
2022-05-18 13:44:59 -0700

    Added functionality to view all ads and a single ad. Also removed functionality to delete property from cloudinary when marked as sold

--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -4,7 +4,9 @@ const verifyToken = require('../middlewares/verifyToken');
 const upload = require('../config/multer');
 
 router.post('/api/v1/property/create', upload.single('image'), verifyToken, propertyController.addProperty);
-router.put('/api/v1/property/:image_id', verifyToken, propertyController.updateSold);
+router.put('/api/v1/property/:id', verifyToken, propertyController.updateSold);
 router.delete('/api/v1/property/:image_id', verifyToken, propertyController.deleteProperty);
+router.get('/api/v1/property/view-all', verifyToken, propertyController.viewAll);
+router.get('/api/v1/property/view/:id', verifyToken, propertyController.viewOne);
 
 module.exports = router;
